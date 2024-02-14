5d5ad8eef5d525aa8327031cde52b79dbe9a89f8
kz91 <kznyan91@gmail.com>
2024-02-14 21:14:37 +0900

    不要な機能の削除

--- LineCanvas.py
+++ LineCanvas.py
@@ -15,10 +15,6 @@ class Application(tkinter.Frame):
     def create_widgets(self):
         self.vr = tkinter.IntVar()
         self.vr.set(1)
-        self.write_radio = tkinter.Radiobutton(self, text='write', variable=self.vr, value=1, command=self.change_radio)
-        self.write_radio.grid(row=0, column=0)
-        self.erase_radio = tkinter.Radiobutton(self, text='erase', variable=self.vr, value=2, command=self.change_radio)
-        self.erase_radio.grid(row=0, column=1)
 
         self.clear_button = tkinter.Button(self, text='clear all', command=self.clear_canvas)
         self.clear_button.grid(row=0, column=2)
@@ -35,15 +31,8 @@ class Application(tkinter.Frame):
         self.old_x = None
         self.old_y = None
         self.color = 'black'
-        self.eraser_on = False
         self.im = Image.new('RGB', (600, 600), 'white')
         self.draw = ImageDraw.Draw(self.im)
-
-    def change_radio(self):
-        if self.vr.get() == 1:
-            self.eraser_on = False
-        else:
-            self.eraser_on = True
 
     def clear_canvas(self):
         self.test_canvas.delete(tkinter.ALL)
@@ -53,10 +42,7 @@ class Application(tkinter.Frame):
         subprocess.run(['python', 'CharPredict.py'])
 
     def paint(self, event):
-        if self.eraser_on:
-            paint_color = 'white'
-        else:
-            paint_color = 'black'
+        paint_color = 'black'
         if self.old_x and self.old_y:
             self.test_canvas.create_line(self.old_x, self.old_y, event.x, event.y, width=5.0, fill=paint_color, capstyle=tkinter.ROUND, smooth=tkinter.TRUE, splinesteps=36)
             self.draw.line((self.old_x, self.old_y, event.x, event.y), fill=paint_color, width=5)
